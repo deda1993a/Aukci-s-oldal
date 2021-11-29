ff405cc88b806a60453ea84459397eb40eca15b9
David Demeter <demeterd1993@hotmail.com>
2021-11-29 17:52:53 +0100

    #2

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace aukcio.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IEnumerable<Person> OnPost(Person person)
+        {
+            Console.WriteLine($"name = {person.Name}");
+            Console.WriteLine($"password = {person.Password}");
+
+            var result=new List<Person>
+            {
+               
+              person
+                
+            };
+            return result.ToArray();  
+        }
+
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,6 @@
+using System;
+
+public class Person{
+    public string Name {get;set;}
+    public string Password {get;set;}
+}
